a7e03c31b79659cbbedcba3e8af8444d8337d4ba
The-Hawkeye <82801422+The-Hawkeye@users.noreply.github.com>
2026-03-19 12:14:15 +0530

    update env

--- src/search.py
+++ src/search.py
@@ -1,8 +1,9 @@
 from tavily import TavilyClient
 import os
-
+import streamlit as st
+tavily_key = st.secrets["TAVILY_API_KEY"]
 def search_web(query: str, top_k: int = 5):
-    client = TavilyClient(api_key=os.getenv("TAVILY_API_KEY"))
+    client = TavilyClient(api_key=tavily_key)
 
     res = client.search(
         query=query,
